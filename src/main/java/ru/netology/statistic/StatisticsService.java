a7952e00b1a17767bb28451ad7b2d77601100006
Красовский АВ <AV.Krasovskiy@ya.ru>
2023-04-26 17:04:31 +0700

    StatisticsService tested

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -4,10 +4,20 @@ public class StatisticsService {
     public long findMax(long[] incomesInBillions) {
         long currentMax = incomesInBillions[0];
         for (long income : incomesInBillions) {
-            if (income > currentMax) {
+            if (income >= currentMax) {
                 currentMax = income;
             }
         }
         return currentMax;
+    }
+
+    public long findMin(long[] incomesInBillions) {
+        long currentMin = incomesInBillions[0];
+        for (long income : incomesInBillions) {
+            if (income < currentMin) {
+                currentMin = income;
+            }
+        }
+        return currentMin;
     }
 }
